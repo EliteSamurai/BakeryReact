255471474b1c406a83ca46f5e4b06edcfe1d6871
EliteSamurai <86984995+EliteSamurai@users.noreply.github.com>
2023-07-14 05:46:03 -0500

    Changed completion.html location

--- netlify/functions/completion.js
+++ netlify/functions/completion.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
 exports.handler = (event, context, callback) => {
-  const completionHtmlPath = path.resolve(__dirname, '../../src/stripe/completion.html');
-  
+  const completionHtmlPath = path.resolve(__dirname, '../completion.html');
+
   fs.readFile(completionHtmlPath, 'utf8', (err, data) => {
     if (err) {
       callback(null, {
@@ -19,5 +19,9 @@ exports.handler = (event, context, callback) => {
     }
   });
 };
+
+
+
+
 
 
